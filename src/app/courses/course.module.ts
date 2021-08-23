9cac0563b7da4f39e1b0841be5183cbdb51d0d97
Allyson Halley <allysonhalley@gmail.com>
2021-08-23 17:20:09 -0300

    segregation module

---
+++ src/app/courses/course.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CourseListComponent } from './course-list.component';
+import { CourseInfoComponent } from './course-info.component';
+import { ReplacePipe } from '../pipe/replace.pipe';
+import { StarComponent } from '../star/star.component';
+
+@NgModule({
+  declarations: [
+    CourseListComponent,
+    CourseInfoComponent,
+    ReplacePipe,
+    StarComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    RouterModule.forChild([
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      }
+    ])
+  ]
+})
+export class CourseModule {
+
+}
