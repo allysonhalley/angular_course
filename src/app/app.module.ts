8f29a879c1c843e5838b4eac71c3edb6df847597
Allyson Halley <allysonhalley@gmail.com>
2021-08-23 18:34:19 -0300

    ended module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { Error404Component } from './404/error-404.component';
 import { CourseModule } from './courses/course.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Error404Component,
+    AppComponent
   ],
   imports: [
     HttpClientModule,
@@ -18,9 +16,6 @@ import { CoreModule } from './core/core.module';
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
     ])
   ],
